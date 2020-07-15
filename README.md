7ea6499926251e4b00a44eaebeb5464b0cad3dfc
osuossu8 <osuosuossu18@gmail.com>
2020-07-15 17:48:33 +0000

    start project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+# Setup
+
+- git clone this repo
+
+- ```chmod 777 scripts/download_data.sh```
+
+- ```./scripts/download_data.sh```
 
---
+++ scripts/download_data.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+mkdir ./data
+curl https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv -o "./data/titanic.csv"
+
