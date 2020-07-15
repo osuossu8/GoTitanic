268ee290be49cbb754cf0c8d37d4f2649ee8ec87
osuossu8 <osuosuossu18@gmail.com>
2020-07-15 19:03:32 +0000

    run sample code

---
+++ predict_breast_cancer_model.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dmitryikh/leaves"
+	"github.com/dmitryikh/leaves/mat"
+	"github.com/dmitryikh/leaves/util"
+)
+
+func main() {
+        // https://godoc.org/github.com/dmitryikh/leaves
+	// loading test data
+	test, err := mat.DenseMatFromCsvFile("data/breast_cancer_test.tsv", 0, false, "\t", 0.0)
+	if err != nil {
+		panic(err)
+	}
+
+	// loading model
+	model, err := leaves.LGEnsembleFromFile("data/lg_breast_cancer.model", true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Name: %s\n", model.Name())
+	fmt.Printf("NFeatures: %d\n", model.NFeatures())
+	fmt.Printf("NOutputGroups: %d\n", model.NOutputGroups())
+	fmt.Printf("NEstimators: %d\n", model.NEstimators())
+	fmt.Printf("Transformation: %s\n", model.Transformation().Name())
+
+	// loading true predictions as DenseMat
+	truePredictions, err := mat.DenseMatFromCsvFile("data/lg_breast_cancer_true_predictions.txt", 0, false, "\t", 0.0)
+	if err != nil {
+		panic(err)
+	}
+	truePredictionsRaw, err := mat.DenseMatFromCsvFile("data/lg_breast_cancer_true_predictions_raw.txt", 0, false, "\t", 0.0)
+	if err != nil {
+		panic(err)
+	}
+
+	// preallocate slice to store model predictions
+	predictions := make([]float64, test.Rows*model.NOutputGroups())
+	// do predictions
+	model.PredictDense(test.Values, test.Rows, test.Cols, predictions, 0, 1)
+	// compare results
+	const tolerance = 1e-6
+	if err := util.AlmostEqualFloat64Slices(truePredictions.Values, predictions, tolerance); err != nil {
+		panic(fmt.Errorf("different predictions: %s", err.Error()))
+	}
+
+	// compare raw predictions (before transformation function)
+	rawModel := model.EnsembleWithRawPredictions()
+	rawModel.PredictDense(test.Values, test.Rows, test.Cols, predictions, 0, 1)
+	if err := util.AlmostEqualFloat64Slices(truePredictionsRaw.Values, predictions, tolerance); err != nil {
+		panic(fmt.Errorf("different raw predictions: %s", err.Error()))
+	}
+	fmt.Println("Predictions the same!")
+}
